b4ba0972671ac22679e70b5785d5b300350aa2f0
Kristian Koehntopp <kris-git@koehntopp.de>
2022-11-26 21:35:06 +0100

    Expire old backups.

--- backup-vaultwarden.py
+++ backup-vaultwarden.py
@@ -3,6 +3,7 @@
 from shutil import copytree, ignore_patterns, make_archive, rmtree
 from pathlib import Path
 from datetime import datetime
+import time
 import sys
 import subprocess
 
@@ -34,26 +35,26 @@ class Backup:
 
         self.debug = debug
         self.datadir = Path(datadir)
-        self.backupdir = Path(backupdir) / f"backup-{self.now}"
+        self.backupdir = Path(backupdir)
+        self.stagedir = self.backupdir / f"backup-{self.now}"
 
     def make_staging(self):
         """ Create the staging directory. """
         if self.debug:
-            print(f"Making staging {self.backupdir}")
-        Path(self.backupdir).mkdir(parents=True, exist_ok=False)
+            print(f"Making staging {self.stagedir}")
+        Path(self.stagedir).mkdir(parents=True, exist_ok=False)
 
     def cleanup_staging(self):
         """ Remove the staging directory. """
         if self.debug:
-            print(f"Remove staging {self.backupdir}.")
-        rmtree(self.backupdir, ignore_errors=False)
-
+            print(f"Remove staging {self.stagedir}.")
+        rmtree(self.stagedir, ignore_errors=False)
 
     def backup_db(self):
         """ Make a backup of the sqlite3 database separately, using the .backup command for database backups. """
 
         data_dbfile = self.datadir / "db.sqlite3"
-        backup_dbfile = self.backupdir / "db.sqlite3"
+        backup_dbfile = self.stagedir / "db.sqlite3"
         cmd = [self.sqlite3, data_dbfile, f".backup {backup_dbfile}"]
         if self.debug:
             print(f"backup_db: {cmd=}")
@@ -67,15 +68,15 @@ class Backup:
     def backup_everything_else(self):
         """ Using copytree(), we make a copy of all things except the database. """
         if self.debug:
-            print(f"Copy files from {self.datadir} to {self.backupdir}.")
-        copytree(self.datadir, self.backupdir, dirs_exist_ok=True, ignore=ignore_patterns('db.sqlite3*'))
+            print(f"Copy files from {self.datadir} to {self.stagedir}.")
+        copytree(self.datadir, self.stagedir, dirs_exist_ok=True, ignore=ignore_patterns('db.sqlite3*'))
 
     def backup_bztar(self):
         """ Compress the staging directory into a .tar.bz2. """
         if self.debug:
-            print(f"Archive {self.backupdir} into {self.backupdir}.tar.bz2.")
+            print(f"Archive {self.stagedir} into {self.stagedir}.tar.bz2.")
 
-        make_archive(str(self.backupdir), "bztar", self.backupdir)
+        make_archive(str(self.stagedir), "bztar", self.stagedir)
 
     def backup(self):
         self.make_staging()
@@ -86,6 +87,22 @@ class Backup:
         self.backup_bztar()
         self.cleanup_staging()
 
+    def expire(self, days=365.0):
+        """ Expire Backups older than days (default: 365). """
+        now = time.time()
+        cutoff = now - days * 86400
+
+        for p in self.backupdir.glob("*.tar.bz2"):
+            ctime = p.stat().st_ctime
+            if ctime < cutoff:
+                if self.debug:
+                    print(f"Expire file {p} ({ctime=} {cutoff=}).")
+                p.unlink()
+            else:
+                if self.debug:
+                    print(f"File {p} still good ({ctime=} {cutoff=}).")
+
 
 b = Backup()
 b.backup()
+b.expire()
